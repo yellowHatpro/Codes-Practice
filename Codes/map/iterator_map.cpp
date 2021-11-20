3fbb30f90139291405f0b03bfb3b75fc3311f5e0
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2021-11-20 13:12:00 +0530

    maps

---
+++ Codes/map/iterator_map.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+unordered_map<string , int> newmap;
+newmap["abc"] =1;
+newmap["abc1"] =2;
+newmap["abc2"] =3;
+newmap["abc3"] =4;
+newmap["abc4"] =5;
+newmap["abc5"] =6;
+newmap["abc6"] =7;
+unordered_map<string,int>:: iterator itr;
+itr=newmap.begin();
+while(itr!=newmap.end()){
+	cout<<"Key: "<<itr->first<<" Value: "<<itr->second<<endl;
+	itr++;
+}
+cout<<endl;
+	return 0;
+}
