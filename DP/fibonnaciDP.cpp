218195c1a77ef597a0352964ab8f7c1536961e51
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2021-11-24 20:30:40 +0530

    DP fibonacci

---
+++ DP/fibonnaciDP.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+//Bery yikes 2^n Time
+int fibbonacci(int n){
+    if(n<=1){
+        return n ;
+    }
+    return fibbonacci(n-1)+fibbonacci(n-2);
+}
+//Memoization : Top Down Approach to save our previous answers
+
+int dp_helper(int n, int *ans){
+if (n<=1){
+    return 1;
+    //Check if output already exists
+    if(ans[n]!=-1){
+        return ans[n];
+    }
+    int a = dp_helper(n-1 ,ans);
+    int b = dp_helper(n-2 , ans);
+    //save output for future use
+    ans[n]=a+b;
+    //Return final output
+    return ans[n];
+}
+int fibbonacci_dp(int n){
+    int *ans = new int[n+1];
+    for(int i =0 ; i<=n ; i++){
+        ans[i] = -1;
+    }
+    return dp_helper(n,ans);
+}
+// ITERATIVE SOLUTION//
+//Dynamic Programming : bottom up approach iterative
+int fib0_iter_dp(int n){
+int *ans = new int[n+1];
+ans[0] = 0;
+ans[1]= 1;
+for (int i = 0; i < =n; i++)
+{
+    ans[i] = ans[i-1]+ ans[i+2];
+}
+return ans[n];
+
+}
