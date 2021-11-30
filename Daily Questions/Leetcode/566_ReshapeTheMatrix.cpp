eaf627c0e072bd51ab7676487c002b99b824301f
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2021-11-30 09:16:31 +0530

    Leetcode 566 Matrix q ez

---
+++ Daily Questions/Leetcode/566_ReshapeTheMatrix.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+      vector<int> result;
+        vector<vector<int>> res( r , vector<int> (c));
+           if((r*c)!=(mat.size()*mat[0].size())) return mat;
+        for(int i = 0 ; i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+                result.push_back(mat[i][j]);
+            }
+        }
+        //Using Iterator is must.
+        auto it = result.begin();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                res[i][j]= *it;
+                it++;
+            }
+        }
+        return res;
+    }
+};
