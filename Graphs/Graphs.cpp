42c6337db8483fc1f83685e3188e35d88ce81b05
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2021-12-06 12:36:33 +0530

    DFS Traversal Graph

---
+++ Graphs/Graphs.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;   
+//DFS traversal of the graph
+void print(int** edges , int n, int sv , bool* visited){
+    cout<<sv<<endl;
+    visited[sv] = true;
+    for(int i=0;i<n;i++){
+        if(edges[sv][i]==1){
+            if(visited[i]){
+                continue;}
+            print(edges,n,i,visited);
+        }
+    }
+    
+}
+
+int main()
+{
+    int n;
+    int e;
+    cin >> n >> e;
+    int** edges = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;//0 means no edge
+        }
+        
+    }
+    for (int i = 0; i < e; i++)
+    {
+        int f,s;
+        cin>>f>>s;
+        edges[f][s] = 1;//first to second edge
+        edges[s][f] = 1;//second to first edge
+    }
+    bool *visited = new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i] = false;
+    }
+
+    print(edges,n,0,visited);
+    
+    
+}
