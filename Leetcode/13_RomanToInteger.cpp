a1ca166fbf58bdee4e504f419ef4e5c519e51c14
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-03-24 19:53:54 +0530

    leet daily

--- Leetcode/13_RomanToInteger.cpp
+++ Leetcode/13_RomanToInteger.cpp
@@ -4,6 +4,27 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-      
+        map<char,int> m;
+        int sum=0;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        int i;
+        for ( i = s.length()-2;  i >=0; i--){
+           if (m[s[i+1]]>m[s[i]])
+            {sum+=m[s[i+1]];
+              sum-=m[s[i]];
+              i--;
+            } 
+            else{
+              sum+=m[s[i+1]];
+            }
+        }
+        if (i==-1) sum+=m[s[0]];
+        return sum;
       }
 };
