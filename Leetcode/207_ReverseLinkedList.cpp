065c3537fd30cdd7644756952fd5310d7e469e39
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-04-02 20:25:55 +0530

    Daily leet

---
+++ Leetcode/207_ReverseLinkedList.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+        ListNode* smallAns = reverseList(head->next);
+        ListNode* temp = smallAns;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next = head;
+        head->next = NULL;
+        return smallAns;
+    }
+};
