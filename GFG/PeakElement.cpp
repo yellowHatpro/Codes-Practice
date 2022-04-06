539c739d3dfcb94500c8082cbe0c9ed10c947e8f
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-04-06 10:55:15 +0530

    gfg cool q done

---
+++ GFG/PeakElement.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       int l = 0;
+       int r = n-1;
+        if (n==1) return 0;
+       if(arr[0]>=arr[1]) return 0;
+       if (arr[n-1]>arr[n-1]) return n-1;
+       
+       while(l<=r){
+       	int mid = (l+r)/2;
+      
+       	if (arr[mid]>=arr[mid-1]&&arr[mid]>=arr[mid+1])
+       	{
+       		return mid;
+       	}
+       	else if(arr[mid-1]>=arr[mid]){
+       		r=mid-1;
+       	}
+       	else{
+       		l=mid+1;
+       	}
+       
+    }
+}
+};
