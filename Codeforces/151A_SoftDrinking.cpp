0b7738b01ea14384a505c1773e9b811eb9ccf527
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-04-10 23:39:12 +0530

    daily cf

---
+++ Codeforces/151A_SoftDrinking.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;cin>>n;
+	int k;cin>>k;
+	int l;cin>>l;
+	int c;cin>>c;
+	int d;cin>>d;
+	int p;cin>>p;
+	int nl;cin>>nl;
+	int np;cin>>np;
+	int kxl = k*l;
+	int enuf = kxl/n;
+	int enuflime = c*d;
+	int salt = p/np;
+	int res = min({enuf,enuflime,salt})/n;
+	cout<<res;
+cout<<endl;
+	return 0;
+}
