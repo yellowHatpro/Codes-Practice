2d9d93d0873e794a62c5bf97862dc47efdc46516
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-04-27 22:52:09 +0530

    Daily leet

---
+++ Leetcode/78_Subsets.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+	vector<vector<int>> output;
+	int n;
+
+	void backtrack(int first, vector<int> v, vector<int> nums, int k){
+		if (v.size()==k)
+		{
+			output.push_back(v);
+			return;
+		}
+		for (int i = first; i < n; ++i)
+		{
+			v.push_back(nums[i]);
+			backtrack(i+1,v, nums,k);
+			v.pop_back();
+		}
+	}
+
+	vector<vector<int>> subsets(vector<int>& nums) {
+	 	n= nums.size();
+	 	for (int i = 0; i < n+1; ++i)
+	 	{	vector<int> v;
+	 		backtrack(0,v,nums,i);
+	 	}
+	 	return output;
+	 }
+};
+
+class SolutionRecursive {
+public:
+	vector<vector<int>> subs;
+
+	void subsetsHelper(vector<int>& nums, int n, vector<int> v){
+		if(n==nums.size()){
+			return;
+		}
+		v.push_back(nums[n]);
+		subs.push_back(v);
+		subsetsHelper(nums,n+1,v);
+		v.pop_back();
+		subsetsHelper(nums,n+1,v);
+	}
+    vector<vector<int>> subsets(vector<int>& nums) {
+    	subs.push_back({});
+    	vector<int> v;
+        subsetsHelper(nums,0,v);
+        return subs;
+    }
+};
+
+int main(){
+	SolutionRecursive s;
+	vector<int> nums = {1,2,3};
+	vector<vector<int>> res= s.subsets(nums);
+
+	for (int i = 0; i < res.size(); ++i)
+	{
+		for (int j = 0; j < res[i].size(); ++j)
+		{
+			cout<<res[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
