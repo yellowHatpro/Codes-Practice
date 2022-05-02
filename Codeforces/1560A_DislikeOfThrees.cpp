ce5ea564cf3d0f762afdea615917b46616deb7f8
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-02 21:46:15 +0530

    daily cf

---
+++ Codeforces/1560A_DislikeOfThrees.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int k;cin>>k;
+	int x=0;
+	for (int i = 1; i <= k; ++i)
+	{
+		if (x%3==0 || x%10==3)
+			{
+				x++;
+				if (x%3==0 || x%10==3)
+				{
+					x++;
+				}
+			
+			}
+			x++;	
+	}
+	cout<<x-1<<endl;
+	return;
+}
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+}
