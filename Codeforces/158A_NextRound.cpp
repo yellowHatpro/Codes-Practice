4731b5240b943d8bffbffa4a0fd0eb7617c69f67
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-15 23:07:37 +0530

    daily cf

--- Codeforces/158A_NextRound.cpp
+++ Codeforces/158A_NextRound.cpp
@@ -4,15 +4,21 @@ int main(){
 	int n,k;
 	cin>>n>>k;
 	int cnt=0;
+	vector<int> v(n);
+	int kcheck = 0;
 	for (int i = 0; i < n; ++i)
 	{
-		int x;cin>>x;
-		if (x>=k && k!=0)
+		cin>>v[i];
+	}
+	kcheck = v[k-1];
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (v[i]>=kcheck && v[i]!=0)
 		{
 			cnt++;
 		}
-		
 	}
-		cout<<cnt<<endl;
+	cout<<cnt<<endl;
 	return 0;
 }
