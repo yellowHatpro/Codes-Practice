e1b50827b3675745c692843dd635face35d280a5
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-16 23:10:25 +0530

    daily leet

---
+++ Leetcode/35_SearchInsertPosition.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        int mid = 0;
+        while(lo<=hi){
+        	mid = hi - (hi-lo)/2;
+        	if (nums[mid]==target)
+        	{
+        		return mid;
+        	}
+        	else if (nums[mid]<target){
+        		lo=mid+1;
+        	}
+        	else{
+        		hi = mid-1;
+        	}
+        }
+        return lo;
+    }
+};
