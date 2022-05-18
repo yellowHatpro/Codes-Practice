7eb46cd784afa9f8596ce5566eb3def844dd9d79
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-18 20:02:06 +0530

    daily leet

---
+++ Leetcode/744_FindSmallestLetterGreatestThanTarget.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int lo = 0;
+        int hi = letters.size();
+    
+        while(lo<hi){
+            int mi = lo+ (hi-lo)/2;
+          if (letters[mi] <= target) lo = mi + 1;
+            else hi = mi;
+            }
+        return letters[lo%letters.size()];
+        }
+    };
