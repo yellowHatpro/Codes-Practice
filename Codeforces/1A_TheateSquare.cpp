9baab9aab0e68f86409af862958faa003f6e4e2e
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-06-03 22:38:01 +0530

    daily leet

---
+++ Codeforces/1A_TheateSquare.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	float a,b,c;
+	cin>>a>>b>>c;
+	cout<<ceil(a/c)*ceil(b/c)<<endl;
+	return 0;
+}
---
+++ Leetcode/389_FindTheDifference.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i]!=t[i])
+            {
+                return t[i];
+            }
+        }
+        return t[t.length()-1];
+    }
+};
