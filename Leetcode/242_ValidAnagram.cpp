feb2955b1b9a937f9256f62bd432e5d896361ae9
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-06-06 22:38:07 +0530

    daily leet

---
+++ Leetcode/242_ValidAnagram.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+  		map<char,int> m;
+  		for (int i = 0; i < s.length(); ++i)
+  		      {
+  		      	m[s[i]]++;
+  		      }
+  		for (int i = 0; i < t.length(); ++i)
+  		            {
+  		            	if (m[t[i]]==0)
+  		            	{
+  		            		return false;
+  		            	}
+                        else if (m[t[i]]==1){
+                            m.erase(t[i]);
+                        }
+  		            	else {
+  		            		m[t[i]]--;
+  		            	}
+  		            }
+  		return m.size()==0;                        
+    }
+};
+
+class SolutionSort {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if (s==t) return true;
+        else return false;
+    }
+};
