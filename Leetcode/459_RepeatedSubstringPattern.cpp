819f9cae2d59dccea25b6585903227d59c22e8f5
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-06-10 23:12:24 +0530

    daily leet

---
+++ Leetcode/459_RepeatedSubstringPattern.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n=s.size();
+        if(n<=1)
+            return 0;
+        string str="";
+        for(int i=1;i<n;i++){
+            if(s[i]==s[0]){
+                str=s.substr(i,n-i)+s.substr(0,i); // if first character matches we put starting part at the end and check if we get the same string.
+                if(str==s)
+                    return 1;
+                str="";
+            }   
+        }
+        return 0;
+    }
