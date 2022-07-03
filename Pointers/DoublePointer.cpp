e94364ff1c0ef1c22fb69280cddcaec22b7a8f65
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-07-03 18:35:14 +0530

    pointers covered ✅

---
+++ Pointers/DoublePointer.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void increment1(int** p){
+	p++;
+}
+
+void increment2(int** p){
+	(*p)++;
+}
+
+void increment3(int **p){
+	(**p)++;
+}
+
+
+
+
+int main(){
+	int i = 10;
+	int* ptr = &i;
+	int** dptr = &ptr;
+	cout<<"double pointer address"<<dptr<<endl;
+	cout<<"pointer address"<<ptr<<endl;
+
+	cout<<"double pointer value"<<*dptr<<endl;//same as pointer
+
+	cout<<"double pointer containing address of pointer having value "<<**dptr<<endl;
+
+	cout<<"increment functions"<<endl;
+	increment1(dptr);
+	cout<<**dptr<<endl; // no changes
+	increment2(dptr);
+	cout<<**dptr<<endl;// changes the pointer so garbage value acc to new memory location, but value changes
+	increment3(dptr);
+	cout<<**dptr<<endl; // directly affects the integer value so changes ofc
+	return 0;
+}
---
+++ Pointers/PointersWithFunctions.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print (int* i) {
+	cout<<*i<<endl;
+}
+
+//By default arguments are passed by value
+void incrementPointer(int* p){
+	p++;
+}
+
+void increment(int* p){
+	(*p)++;
+}
+
+
+int sum(int a[], int size){
+	//Inside function array is passed as a pointer, and not as an array
+	//cout<<"size to tell if a is array or pointer "<<sizeof(a)<<endl;
+
+	//That's why we can either give the args in the function as : int a[] or int* a..
+	int sum=0;
+	for (int i = 0; i < size; ++i)
+	{
+		sum+=a[i];
+		//sum+= *(a+i); will work fine as well..
+	}
+
+	return sum;
+}
+
+int main(){
+	int x = 10;
+	int* p = &x;
+//	print (p);
+	cout<<p<<endl;
+	incrementPointer(p);
+	cout<<p<<endl;
+
+	cout<<"value before "<<*p<<endl;
+	increment(p);
+	cout<<"value after "<<*p<<endl;
+
+	int a[10]={1,2,3,4,5,6,7,8,9,10};
+	cout<<sizeof(a)<<endl;
+	cout<<sum(a,10)<<endl;
+	//Since the array is passed as a pointer, we can pass the part of an array like this: 
+	cout<<sum(a+3,7)<<endl;
+
+	return 0;
+}
