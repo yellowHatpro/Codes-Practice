a6acf975606d95b6db75c8fedd44942a72933e57
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-07-13 23:58:36 +0530

    Starters 47 DIV 2 contest

---
+++ Codechef/Starters/Starters 47/SimilarHeights.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+	ll n;cin>>n;
+	map<ll, ll> m;
+	vector<ll> v;
+	ll sec=0;
+	ll ssize = 0;
+	ll g = INT_MIN;
+	for (ll i = 0; i < n; ++i)
+	{	
+
+		ll x;cin>>x;
+		v.push_back(x);
+		m[x]++;
+		if (v[i]>g)
+		{
+			g=v[i];
+		}
+	}
+	
+	for(auto i: m){
+		if (i.second==1)
+		{
+			ssize++;
+		}
+		sec = max(sec,i.second);
+
+	}
+	
+	if (ssize%2==0)
+	{
+		cout<<ssize/2<<endl;
+	}
+	else {
+	
+		if (m[g]==1 && sec ==2)
+		{
+			cout<<(ssize/2) +2<<endl;
+			return;
+		}
+		cout<<(ssize/2) +1<<endl;
+	}
+	return;
+}
+
+
+int main(){
+	int t;cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
