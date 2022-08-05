64b1bc5c61618e741070bffdaf8716eda2fc65d3
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-08-06 00:07:09 +0530

    small code change fixed leet issue

--- Leetcode/785_IsGraphBipartite.cpp
+++ Leetcode/785_IsGraphBipartite.cpp
@@ -5,8 +5,13 @@ public:
     bool checkBipartite(vector<vector<int>> &graph, vector<int>&colored, int currentNode){
         if (colored[currentNode]==-1) colored[currentNode] = 1;
         for (auto itr : graph[currentNode]){
-            
+            if (colored[itr]==-1){
+                colored[itr] = 1 - colored[currentNode];
+                if (!checkBipartite(graph, colored, itr)) return false; 
+            }
+            else if (colored[itr]==colored[currentNode]) return false;
         }
+        return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
@@ -14,9 +19,10 @@ public:
         vector<int> colored(V,-1);//not colored in beginning
         for (int i = 0; i < V; i++)
         {
-            if (!checkBipartite(graph,colored,i)) return false;
+            if (colored[i]==-1) {
+                if (!checkBipartite(graph,colored,i)) return false;
+            }
         }
         return true;
-        
     }
 };
