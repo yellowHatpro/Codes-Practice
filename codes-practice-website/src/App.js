da8b91ab74bfe644ef8b9da46f697dde580e1ceb
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-01-01 20:19:04 +0530

    changed favicon

--- codes-practice-website/src/App.js
+++ codes-practice-website/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import DisplayCard from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-      
+
         </div>
       <Footer />
     </div>
