2e33bda744c5232389556f3056777e478d7db1ae
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-02-09 23:45:31 +0530

    cp failure kek

---
+++ Codeforces/851_DIV2/A_OneAndTwo.cpp
@@ -0,0 +1,97 @@
+// yellowhatpro
+#include <bits/stdc++.h>
+using namespace std;
+
+//Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+//Constants
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9+7;
+
+//Macros
+#define FIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl "\n"
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define len(x) ((int) x.size())
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define mid(l,r) (l+(r-l)/2)
+#define sortall(x) sort(all(x))
+#define print(a) for(auto i: a) cout<<i<<" "; cout<<endl
+
+//Type define
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+
+//Math Functions
+ll LCM(int a, int b){
+	return ((ll)a*b)/__gcd(a,b);
+}
+
+ll modpow(ll x, ll n, int m=MOD){
+	if (x==0 && n==0) return 0; 
+	ll res = 1;
+	while(n>0){
+		if (n%2){
+			res = (res*x) %m;
+		}
+		x = (x*x)%m;
+		n/=2;
+	}
+	return res;
+}
+
+void solve(){
+	int n;cin>>n;
+	map<int, int> m;
+	vector<int> v(n);
+	for (int i = 0; i < n; ++i)
+		{
+			int x;cin>>x;
+			v[i] = x;
+			m[x]++;
+		}
+		if(m[2]%2!=0){
+		cout<<-1<<endl;
+		return;
+	}
+	if (m[1]==0){
+		cout<<n/2<<endl;
+		return;
+	} else if (m[2]==0){
+		cout<<1<<endl;
+		return;
+	}
+	
+	int k = 0;
+	int steps = m[2]/2;
+	while(steps>0){
+		if (v[k++]==2){
+			steps--;
+		}
+		
+	}
+	cout<<k<<endl;
+	return;
+}
+
+int32_t main(){
+	FIO
+	int t=1;
+	cin>>t;
+	while(t--){
+	solve();
+	}
+	return 0;
+}
---
+++ Codeforces/Edu/2 Pointer/Step2_A_SegmentWithSmallSum.cpp
@@ -0,0 +1,88 @@
+// yellowhatpro
+#include <bits/stdc++.h>
+using namespace std;
+
+//Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+//Constants
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9+7;
+
+//Macros
+#define FIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl "\n"
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define len(x) ((int) x.size())
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define mid(l,r) (l+(r-l)/2)
+#define sortall(x) sort(all(x))
+#define print(a) for(auto i: a) cout<<i<<" "; cout<<endl
+
+//Type define
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+
+//Math Functions
+ll LCM(int a, int b){
+	return ((ll)a*b)/__gcd(a,b);
+}
+
+ll modpow(ll x, ll n, int m=MOD){
+	if (x==0 && n==0) return 0; 
+	ll res = 1;
+	while(n>0){
+		if (n%2){
+			res = (res*x) %m;
+		}
+		x = (x*x)%m;
+		n/=2;
+	}
+	return res;
+}
+
+void solve(){
+	int n,m;cin>>n>>m;
+	vector<int> v(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>v[i];
+	}
+	int l = 0;
+	int ans = 0;
+	int sum = 0;
+	for (int r = 0; r < n; ++r)
+	{
+		sum+=v[r];
+		while(!(sum<=m)){
+			sum-=v[l];
+			l++;
+
+		}
+		ans = max(ans,r-l+1);
+	}
+
+	cout<< ans<<endl;
+	return;
+}
+
+int32_t main(){
+	FIO
+	int t=1;
+	//cin>>t;
+	while(t--){
+	solve();
+	}
+	return 0;
+}
