11c08c201d718c3dee59c171dd48792c0bbbea90
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-02-17 23:49:51 +0530

    cf

---
+++ Codeforces/1790A_PolycarpAndTheDayOfPi.cpp
@@ -0,0 +1,76 @@
+// yellowhatpro
+#include <bits/stdc++.h>
+using namespace std;
+
+//Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+//Constants
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9+7;
+
+//Macros
+#define FIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl "\n"
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define len(x) ((int) x.size())
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define mid(l,r) (l+(r-l)/2)
+#define sortall(x) sort(all(x))
+#define print(a) for(auto i: a) cout<<i<<" "; cout<<endl
+
+//Type define
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+
+//Math Functions
+ll LCM(int a, int b){
+	return ((ll)a*b)/__gcd(a,b);
+}
+
+ll modpow(ll x, ll n, int m=MOD){
+	if (x==0 && n==0) return 0; 
+	ll res = 1;
+	while(n>0){
+		if (n%2){
+			res = (res*x) %m;
+		}
+		x = (x*x)%m;
+		n/=2;
+	}
+	return res;
+}
+
+void solve(){
+	string s ;cin>>s;
+	string pi = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+	for (int i = 0; i < pi.size(); ++i)
+	{
+		if (s[i]!=pi[i]){
+			cout<<i<<endl;
+			break;
+		}
+	}
+	return;
+}
+
+int32_t main(){
+	FIO
+	int t=1;
+	cin>>t;
+	while(t--){
+	solve();
+	}
+	return 0;
+}
