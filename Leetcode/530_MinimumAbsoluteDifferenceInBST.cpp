fad323dacdcdbf41b79eeb6762b6b4547436462b
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-02-17 22:43:49 +0530

    leety

---
+++ Leetcode/530_MinimumAbsoluteDifferenceInBST.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    int d1 = INT_MAX;
+    int res = INT_MAX;
+    void inorder(TreeNode* root){
+        if (!root) return;
+        if (root->left) inorder(root->left);
+        if (d1!=INT_MAX){
+            res = min(res,root->val - d1);
+        }
+        d1 = root->val;
+        if(root->right) inorder(root->right);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        return res;
+    }
+};
---
+++ Leetcode/746_MinCostClimbingStairs.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+
+    int f(int n, vector<int> &cost, vector<int>& dp){
+        if (n<0) return dp[0] = 0;
+        if (n==0) return dp[n] = cost[0];
+        if (dp[n]!=-1) return dp[n];
+        int a = cost[n] +  f(n-1,cost, dp);
+        int b = cost[n] + f(n-2, cost, dp);
+        return dp[n] = min({a,b});
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+       
+        int n = cost.size();
+         vector<int> dp(n);
+         for (int i = 2; i<n;i++){
+            cost[i] = min(cost[i-1], cost[i-2]); 
+         }
+        return min(f(n-1,cost,dp), f(n-2, cost,dp));
+    }
+};
