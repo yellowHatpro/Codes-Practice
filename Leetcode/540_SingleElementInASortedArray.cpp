1a381429a7dcc35115e126396b57c637295bea4a
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-02-21 23:53:35 +0530

    pause tode

--- Leetcode/540_SingleElementInASortedArray.cpp
+++ Leetcode/540_SingleElementInASortedArray.cpp
@@ -10,3 +10,34 @@ public:
         return x;
     }
 };
+
+class SolutionLogn{
+public:
+	int singleNonDuplicate(vector<int>& nums){
+		int n = nums.size();
+		int l = 0;
+		int r = n-1;
+        if (n==1) return nums[0];
+		while(l<r){
+			int mid = (l+r)/2;
+            if (mid==0 && nums[mid]!=nums[mid+1]) return nums[mid];
+            if (mid)
+			if (nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+			if (mid%2==0){
+				if (nums[mid]==nums[mid-1]){
+					r = mid-1;
+				} else {
+					l = mid+1;
+				}
+			}
+			if (mid%2!=0){
+				if (nums[mid]!=nums[mid-1]){
+					r = mid -1;
+				} else {
+					l = mid+1;
+				}
+			}
+		}
+        return nums[l];
+	}
+};
