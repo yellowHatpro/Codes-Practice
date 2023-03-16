3cd6b257f0aee1877e36c57d14e699cb08835911
yellowhatpro <yellowhatpro3119@gmail.com>
2023-03-16 22:23:56 +0530

    tode leet and cf

---
+++ Leetcode/106_ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
+        if(x > y || a > b)return nullptr;
+        TreeNode *node = new TreeNode(po[b]);
+        int SI = x;  
+        while(node->val != in[SI])SI++;
+        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
+        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
+        return node;
+    }
+    TreeNode* buildTree(vector<int>& in, vector<int>& po){
+        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+    }
+};
