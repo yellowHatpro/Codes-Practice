249edbab774e06409a2bb62e5438309271d722a0
yellowhatpro <yellowhatpro3119@gmail.com>
2023-04-02 22:13:00 +0530

    cf tode aaaa contest

---
+++ Codeforces/862_DIV2/B.cpp
@@ -0,0 +1,93 @@
+// yellowhatpro
+#include <bits/stdc++.h>
+using namespace std;
+
+//Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+//Constants
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9+7;
+
+//Macros
+#define FIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl "\n"
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define len(x) ((int) x.size())
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define mid(l,r) (l+(r-l)/2)
+#define sortall(x) sort(all(x))
+#define print(a) for(auto i: a) cout<<i<<" "; cout<<endl
+
+//Type define
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+
+//Math Functions
+ll LCM(int a, int b){
+	return ((ll)a*b)/__gcd(a,b);
+}
+
+ll modpow(ll x, ll n, int m=MOD){
+	if (x==0 && n==0) return 0; 
+	ll res = 1;
+	while(n>0){
+		if (n%2){
+			res = (res*x) %m;
+		}
+		x = (x*x)%m;
+		n/=2;
+	}
+	return res;
+}
+
+void solve(){
+	int n;cin>>n;
+	string s;cin>>s;
+	char smol = s[0];
+	int ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (s[i]<smol){
+			smol = s[i];
+		}
+	}
+	int reali = 0;
+	while(reali<n && s[reali]==s[0]){
+		reali++;
+	}
+	for (int i = n-1; i >=reali; i--)
+	{
+		if (( s[reali-1]  >s[i] && s[i]==smol) || (s[reali-1]==s[i]  && s[i]==smol)){
+			ans = i;
+			break;
+		}
+	}
+	string news = "" ;
+	news+=s[ans];
+	news+=s.substr(0,ans);
+	news+=s.substr(ans+1, n - ans+1 );
+	cout<<news<<endl;
+
+}
+
+int32_t main(){
+	FIO
+	int t=1;
+	cin>>t;
+	while(t--){
+	solve();
+	}
+	return 0;
+}
