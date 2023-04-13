7262279e25164324e6eb9530032100b6e23d997b
yellowhatpro <yellowhatpro3119@gmail.com>
2023-04-13 23:13:36 +0530

    added cf tode

---
+++ Codeforces/865B_GridReconstruction.cpp
@@ -0,0 +1,93 @@
+// yellowhatpro
+#include <bits/stdc++.h>
+using namespace std;
+
+//Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+//Constants
+constexpr ll INF = 2e18;
+constexpr ld EPS = 1e-9;
+constexpr ll MOD = 1e9+7;
+
+//Macros
+#define FIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl "\n"
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define len(x) ((int) x.size())
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define mid(l,r) (l+(r-l)/2)
+#define sortall(x) sort(all(x))
+#define print(a) for(auto i: a) cout<<i<<" "; cout<<endl
+#define fillv(v) for(auto i: v) cin>>i;
+
+//Type define
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef map<int, int> mii;
+
+//Math Functions
+ll LCM(int a, int b){
+	return ((ll)a*b)/__gcd(a,b);
+}
+
+ll modpow(ll x, ll n, int m=MOD){
+	if (x==0 && n==0) return 0; 
+	ll res = 1;
+	while(n>0){
+		if (n%2){
+			res = (res*x) %m;
+		}
+		x = (x*x)%m;
+		n/=2;
+	}
+	return res;
+}
+
+int CEIL(int a, int b){
+	return (a+b-1)/b;
+}
+
+void solve(){
+	int n;cin>>n;
+	  int ans[3][n + 1];
+        
+    ans[1][1] = 2 * n - 1;
+    ans[2][n] = 2 * n;
+
+    for (int i = 2; i <= n; i++) {
+        if (i % 2 == 0) {
+            ans[1][i] = i;
+            ans[2][i - 1] = i - 1;
+        } else {
+            ans[1][i] = n + (i - 1);
+            ans[2][i - 1] = n + (i - 1) - 1;
+        }
+    }
+	for (int i = 1; i <= 2; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+		{
+			 cout << ans[i][j] << (j == n ? '\n' : ' ');
+		}
+	}
+}
+
+int32_t main(){
+	FIO
+	int t=1;
+	cin>>t;
+	while(t--){
+	solve();
+	}
+	return 0;
+}
