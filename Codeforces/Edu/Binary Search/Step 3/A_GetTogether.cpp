42b41401bc2ab04e1b915547171a2e5ca66a802a
yellowhatpro <yellowhatpro3119@gmail.com>
2023-04-23 00:07:28 +0530

    added cf and dp

--- Codeforces/Edu/Binary Search/Step 3/A_GetTogether.cpp
+++ Codeforces/Edu/Binary Search/Step 3/A_GetTogether.cpp
@@ -1,36 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> x;
-vector<int> v;
-int n;
-bool good(double t){
-	auto maxi = (double)INT_MIN;
-	auto mini = (double)INT_MAX;
-	for (int i = 0; i < n; ++i)
-	{
-		maxi = max(maxi,x[i] - v[i]*t);
-		mini = min(mini, x[i]+ v[i]*t);
-	}
-	return maxi<=mini;
+#define ll long long
+#define pb push_back
+#define loop(a) for(int i = 0; i < a; i++)
+#define loopv(i,a) for (int i = 0; i < a; i++)
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define goog(tno) cout << "Case #" << tno <<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+bool good(int n, vector<int>& x, vector<int>& v, double M) {
+    double L = -1e10;
+    double R = 1e10;
+    for (int i = 0; i < n; i++) {
+        double xl = x[i] - M*v[i];
+        double xr = x[i] + M*v[i];
+
+        L = max(L, xl);
+        R = min(R, xr);
+
+        if (L > R) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int main(){
-	cin>>n;
-	x.resize(n);
-	v.resize(n);
-	for (int i = 0; i < n; ++i)
-	{
-		int xi,vi;cin>>x[i]>>v[i];
-	}
-	double l = 0;
-	double r = 1e10;
-	for (int i = 0; i < 100; ++i)
-	{
-		double m = (l+r)/2;
-		if (good(m)) r = m;
-		else l = m;
-	}
-	cout<< l<< '\n';
-	return 0;
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> x(n), v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> v[i];
+    }
+
+    double L = 0;
+    double R = 1e18;
+
+    for (int i = 0; i < 100; i++) {
+        double M = (L + R) / 2;
+
+        if (good(n, x, v, M)) {
+            R = M;
+        } else {
+            L = M;
+        }
+    }
+
+    cout << setprecision(10) << R << endl;
+
+    return 0;
 }
--- DP/IPL.cpp
+++ DP/IPL.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int DP(){
 	int n;cin>>n;
 	vector<int> games(n);
 	vector<int> dp(n);
@@ -20,4 +20,25 @@ int main(){
 	}
 	cout<<sum-min({dp[n-1],dp[n-2],dp[n-3]});
 	return 0;
+}
+
+long long int f(int n, vector<long long int>&v, vector<long long int>&dp, int i){
+	if (i>=n) return 0;
+	if (dp[i]!=-1) return dp[i];
+	long long int doubleTake = (i+1<n) ? v[i] + v[i+1] + f(n,v,dp,i+3) : 0;
+
+	long long int take = max({v[i]+f(n,v,dp,i+2), doubleTake, f(n,v,dp,i+1) });
+	return dp[i] = take;
+}
+
+
+int32_t main(){
+	int n;cin>>n;
+	vector<long long int> v(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>v[i];
+	}
+	vector<long long int> dp(n,-1);
+	cout<<f(n,v,dp,0)<<endl;
 }
