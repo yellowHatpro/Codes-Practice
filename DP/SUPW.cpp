99b0f13ea3e5afaa018f4839826a961e0ceb795f
yellowhatpro <yellowhatpro3119@gmail.com>
2023-04-22 18:43:05 +0530

    ZCO SUPW memo,dp

--- DP/SUPW.cpp
+++ DP/SUPW.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 
 
-
-int main() {
-	int n;cin>>n;
-	vector<int> minutes;
-	for (int i = 0; i < n; ++i)
-	{
-		int x;cin>>x;
-		minutes.push_back(x);
-	}
+void DP(int n, vector<int>& minutes){
+	
 	vector<int> dp(n);
 	dp[0] = minutes[0];
 	dp[1] = minutes[1];
@@ -20,5 +13,26 @@ int main() {
 		dp[i] = min({dp[i-1],dp[i-2],dp[i-3]}) + minutes[i];
 	}
 	cout<< min({dp[n-1],dp[n-2],dp[n-3]});
+}
+
+int memo(int n, vector<int>& mint, int i,vector<int>&dp){
+	if (i>=n){
+		return 0;
+	}
+	if (dp[i]!=-1) return dp[i];
+	int take = mint[i] + min({memo(n,mint,i+1,dp),memo(n,mint,i+2,dp),memo(n,mint,i+3,dp)});
+	return dp[i] = take;
+}
+
+int main() {
+	int n;cin>>n;
+	vector<int> minutes;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;cin>>x;
+		minutes.push_back(x);
+	}
+	vector<int> dp(n,-1);
+	cout<<min({memo(n,minutes,0,dp),memo(n,minutes,1,dp),memo(n,minutes,2,dp)})<<endl;
 	return 0;
 }
