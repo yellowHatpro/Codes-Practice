ddc55d83916e5408f49f89cc11a32f91dc8efabc
yellowhatpro <yellowhatpro3119@gmail.com>
2023-04-23 23:26:50 +0530

    added stuff

--- Leetcode/1416_RestoreTheArray.cpp
+++ Leetcode/1416_RestoreTheArray.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-#define MOD 1e9+7
-#define ll long long
+#define MOD 1000000007
+#define ll long long int
 using namespace std;
 class Solution {
 public:
 
-	int f(string s, int k, vector<ll>&dp, int n, int i){
-		if (i>=n) return 0;
+	int f(string &s, int k, vector<ll>&dp, int n, int i){
+		if (i==n) return 1;
 		if (s[i]=='0') return 0;
 		if (dp[i]!=-1) return dp[i];
 		ll cnt = 0;
 		ll num = 0;
 		for (int j = i; j < n; ++j)
 		{	
-			num = num*10 + s[j];
+			num = (num*10) +( s[j] - '0');
 			if (num>k){
 				break;
 			}
-			cnt+=f(s,k,dp,n,j+1)%MOD;
+			cnt= (cnt+f(s,k,dp,n,j+1))%MOD;
 
 		}
 		return dp[i] = cnt;
