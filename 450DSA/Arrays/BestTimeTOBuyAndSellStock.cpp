14fc09691e2eb96ab5bccc2a2150e1b7e8cfffdf
yellowhatpro <yellowhatpro3119@gmail.com>
2023-05-02 22:07:50 +0530

    some more 450 dsa today

---
+++ 450DSA/Arrays/BestTimeTOBuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for (int i = 0; i < prices.size(); ++i)
+        {
+        	mini = min(prices[i],mini);
+        	profit = max(profit,prices[i]-mini);
+        }
+        return profit;
+    }
+};
