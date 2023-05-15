4815436a769213fa19d125a355cd314a438470ef
yellowhatpro <yellowhatpro3119@gmail.com>
2023-05-15 22:31:48 +0530

    added more methods for promise

--- Leetcode/30 Days of LC JavaScript Challenge/Day11_2621_Sleep.js
+++ Leetcode/30 Days of LC JavaScript Challenge/Day11_2621_Sleep.js
@@ -1,3 +1,17 @@
 async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
+
+async function sleep(millis) {
+    return new Promise((res,rej) => {
+      try {
+        setTimeout(() => res(5), millis)
+      } catch(err) {
+        rej(err)
+      }
+    })
+  }
+
+  async function sleep(milliseconds) {
+	await new Promise(res => setTimeout(res, milliseconds)); 
+}
