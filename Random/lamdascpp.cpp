319fca02913738bb310ca24f5130570f796e997d
yellowhatpro <yellowhatpro3119@gmail.com>
2023-05-26 11:28:40 +0530

    today' leetcode

---
+++ Random/lamdascpp.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <xutility>
+
+using namespace std;
+
+
+
+
+int main (int argc, char *argv[]) {
+  vector<int> v = {3,4,2,5,1};
+  sort(v.begin(), v.end(), [](const int& a, const int& b) -> bool{
+    return a > b;
+  });
+  for (auto itr: v) {
+    cout<<itr<<endl;    
+  }
+  return 0;
+} 
