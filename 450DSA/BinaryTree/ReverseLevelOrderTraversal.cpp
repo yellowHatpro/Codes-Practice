9e13e92fee2535000c6b2cb30a8bb936709dbb59
yellowhatpro <yellowhatpro3119@gmail.com>
2023-05-29 20:12:43 +0530

    Trees revisited

--- 450DSA/BinaryTree/ReverseLevelOrderTraversal.cpp
+++ 450DSA/BinaryTree/ReverseLevelOrderTraversal.cpp
@@ -49,48 +49,40 @@
 #include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
+
 using namespace std;
 
-class Temp {
-  public:
-    int height;
-    int left_index;
-    int right_index;
-
-    Temp(int h, int li, int ri){
-      height = h;
-      left_index = li;
-      right_index = ri;
-  }
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
 };
 
-int heightOfTheTree(vector<int>& inorder, vector<int>& levelOrder, int N){
-	int maxHeight = 0;
-  queue<Temp> q;
-  if (N>=1){
-    Temp temp(0,0,N-1);
-    q.push(temp);
-  }
-  unordered_map<int , int > m;
-  for (int i = 0 ; i<N; i++) {
-    m[inorder[i]] = i;
-  }
-  for (int i = 0 ; i<N ; i++) {
-    Temp temp = q.front();
-    q.pop();
-    maxHeight = max(temp.height, maxHeight);
-    int li = temp.left_index;
-    int ri = temp.right_index;
-    int root_index;
-    root_index = m[levelOrder[i]];
-    if (root_index-1 >=li){
-      Temp left_sub_tree(temp.height+1,li,root_index-1);
-      q.push(left_sub_tree);
+vector<int> reverseLevelOrder(Node *root)
+{
+  stack<vector<int>> s;
+  vector<int> v;
+  queue<Node*> q;
+  q.push(root);
+  while (!q.empty()) {
+    int n = q.size();
+    vector<int> currRow;
+    for(int i=0; i<n;i++){
+      auto node = q.front();
+      q.pop();
+      currRow.push_back(node->data);
+      if (node->left) q.push(node->left);
+      if (node->right) q.push(node->right);
     }
-    if (root_index +1<=ri){
-      Temp right_sub_tree(temp.height+1,root_index+1,ri);
-      q.push(right_sub_tree);
-    }
+    s.push(currRow);
   }
-  return maxHeight;
+  while(!s.empty()){
+    auto stop = s.top();
+    for (auto itr  : stop ) {
+      v.push_back(itr); 
+    }
+    s.pop();
+  }
+  return v;
 }
