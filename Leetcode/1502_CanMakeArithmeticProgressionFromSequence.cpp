942b7f5e441e1fae6bc8db876d214314543e7ccf
yellowhatpro <yellowhatpro3119@gmail.com>
2023-06-06 21:24:25 +0530

    daily leet

---
+++ Leetcode/1502_CanMakeArithmeticProgressionFromSequence.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+       sort(arr.begin(),arr.end());
+       int diff = arr[1] - arr[0];
+       for (auto  i = 2; i < arr.size(); i++) {  
+          if (arr[i] - arr[i-1]!= diff) return 0;
+       }
+        return 1;
+    }
+};
