d8179903111aef8e77518a09d5587cd0399b866b
yellowhatpro <yellowhatpro3119@gmail.com>
2023-06-17 23:36:28 +0530

    inorder postorder unique bt

--- Leetcode/106_ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
+++ Leetcode/106_ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -10,16 +10,21 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
+    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b, map<int, int> &inMap){
         if(x > y || a > b)return nullptr;
         TreeNode *node = new TreeNode(po[b]);
-        int SI = x;  
-        while(node->val != in[SI])SI++;
-        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
-        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
+        int inRoot = inMap[node->val];
+        int numsLeft =inRoot-x;
+        node->left  = Tree(in,x,inRoot-1,po,a,a+numsLeft-1, inMap);
+        node->right = Tree(in,inRoot+1,y,po,a+numsLeft,b-1, inMap);
         return node;
     }
     TreeNode* buildTree(vector<int>& in, vector<int>& po){
-        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+        map<int,int> inMap;
+        for (int i = 0; i < in.size(); ++i)
+        {
+            inMap[in[i]] = i;
+        }
+        return Tree(in,0,in.size()-1,po,0,po.size()-1, inMap);
     }
 };
