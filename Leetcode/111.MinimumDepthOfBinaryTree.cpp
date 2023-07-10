57f7afaeb3b6a319f2ffea663b8b4d9766e83fae
yellowhatpro <yellowhatpro3119@gmail.com>
2023-07-10 23:07:03 +0530

    leetcode

---
+++ Leetcode/111.MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (!root ) {
+            return 0;
+        }
+        if (!root->left) {
+            return 1 + dfs(root->right);
+        } else if (!root->right) {
+            return 1 + dfs(root->left);
+        }
+        
+        return 1 + min(dfs(root->left), dfs(root->right));
+    }
+    
+    int minDepth(TreeNode* root) {
+        return dfs(root);
+    }
+};
