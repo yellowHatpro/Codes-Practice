70deab9b8d9626b26b9f35cfcd27fb3732537025
yellowhatpro <yellowhatpro3119@gmail.com>
2023-07-19 22:31:40 +0530

    dsa

--- Leetcode/435_NonOverlappingIntervals.cpp
+++ Leetcode/435_NonOverlappingIntervals.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+#include <iterator>
 using namespace std;
 
 bool comp(vector<int>& a, vector<int>& b) {
     return a[1] < b[1];
 }
-class Solution {
+class SolutionGreedy {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
     sort(intervals.begin(), intervals.end(), comp);
@@ -17,6 +18,37 @@ public:
       }
     }
     return cnt;
+    }
+};
+
+
+class Solution {
+public:
+    
+    int bs(vector<vector<int>>& intervals, int x, int l, int r){
+      while(l<r){
+		    int m = (l+r)/2;
+		    if (intervals[m][0]>=x){
+          r = m;
+          }
+		    else l = m+1;
+      }
+      return l;
+  }
+
+    int f(vector<int>& dp, vector<vector<int>>& intervals, int i){
+      if (i==intervals.size()) return 0;
+      if (dp[i]!=-1) return dp[i];
+      int nextIndex = bs(intervals,intervals[i][1], i+1, intervals.size());
+      int take = 1 + f(dp,intervals,nextIndex);
+      int notTake = f(dp,intervals,i+1);
+      return dp[i] =  max(take,notTake);
+  }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+      sort(intervals.begin(), intervals.end());
+      vector<int> dp(1e6,-1); 
+      return intervals.size() -  f(dp,intervals,0);
     }
 };
 
---
+++ SDE Sheet/SubsetSums.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+
+    void f(vector<int>& arr, int n, int i, int sum, vector<int>& subsetSum){
+      if (i==n) {
+        subsetSum.push_back(sum);
+        return;
+      }
+     f(arr,n,i+1,sum+arr[i], subsetSum);
+     f(arr,n,i+1,sum,subsetSum);
+    }
+    
+   
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+      
+    vector<int> subsetSum;
+    f(arr,N,0,0,subsetSum);
+    sort(subsetSum.begin(),subsetSum.end());
+    return subsetSum;
+    }
+};
+
