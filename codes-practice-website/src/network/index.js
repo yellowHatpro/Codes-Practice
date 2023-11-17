ba9421bfdccd7e63438a9a32ee8bc98f514d8a9b
yellowhatpro <yellowhatpro3119@gmail.com>
2023-11-17 19:03:38 +0530

    stuff

--- codes-practice-website/src/network/index.js
+++ codes-practice-website/src/network/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const authHeaders = {
-    'Authorization': `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    'Accept': 'application/vnd.github.v3+json'
+  'Authorization': `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  'Accept': 'application/vnd.github+json'
 }
 
 export const fetchRepositoryData = async (url) => {
-    const res = await axios.get(url, {headers: authHeaders})
-    return res.data
+  const res = await axios.get(url, { headers: authHeaders })
+  return res.data
 }
