0f2919c7ad3c0c3f6feb33499493a79b37c84f8a
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-21 23:41:52 +0530

    leetcode tode

--- Leetcode/198_HouseRobber.cpp
+++ Leetcode/198_HouseRobber.cpp
@@ -2,34 +2,36 @@
 using namespace std;
 class Solution {
 public:
+  int f(vector<int> &nums, int i, vector<int> &dp) {
+    if (i == 0)
+      return nums[0];
+    if (i < 0)
+      return 0;
+    if (dp[i] != -1)
+      return dp[i];
+    int take = f(nums, i - 2, dp) + nums[i];
+    int notTake = f(nums, i - 1, dp);
+    return dp[i] = max(take, notTake);
+  }
 
-	int f(vector<int> &nums, int i, vector<int> &dp){
-		if (i==0) return nums[0];
-		if (i<0) return 0;
-		if (dp[i]!=-1) return dp[i];
-		int take = f(nums, i-2, dp) + nums[i];
-		int notTake = f(nums, i-1,dp);
-		return dp[i] = max(take, notTake);
-	}
-
-
-    int rob(vector<int>& nums) {
-    	int n = nums.size();
-    	vector<int> dp(n,-1);
-        return f(nums,n-1,dp);
-    }
+  int rob(vector<int> &nums) {
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return f(nums, n - 1, dp);
+  }
 };
-class SolutionDP{
+class SolutionDP {
 public:
-	int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (nums.size()==1) return nums[0];
-        vector<int> dp(n,0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for (int i = 2; i<n;i++){
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        }
-        return dp[n-1];
+  int rob(vector<int> &nums) {
+    int n = nums.size();
+    if (nums.size() == 1)
+      return nums[0];
+    vector<int> dp(n, 0);
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for (int i = 2; i < n; i++) {
+      dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
     }
+    return dp[n - 1];
+  }
 };
