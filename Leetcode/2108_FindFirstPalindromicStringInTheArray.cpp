5d77ef7fd54b4ab96f2fb6ad53793d7388230ce0
yellowhatpro <yellowhatpro3119@gmail.com>
2024-02-13 08:04:45 +0530

    leetcode

---
+++ Leetcode/2108_FindFirstPalindromicStringInTheArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  string firstPalindrome(vector<string> &words) {
+    for (auto i : words) {
+      string s = i; // store the string s
+      reverse(s.begin(),
+              s.end()); // reverse it example string i=`aba` reverse it s='aba'
+                        // is i and s is same then this is a palindrome
+      if (s == i) {
+        return s;
+      }
+    }
+    return "";
+  }
+};
--- codes-practice-website/src/pages/dir/Directories.jsx
+++ codes-practice-website/src/pages/dir/Directories.jsx
@@ -1,67 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import DisplayCard from "../../components/Card/Card.jsx";
-import File from "../files/File"
+import File from "../files/File";
 import "./Directories.css";
 import { authHeaders } from "../../network/index.js";
 import axios from "axios";
-import { NETWORK_ERROR_CODE } from '../../constants.js';
+import { NETWORK_ERROR_CODE } from "../../constants.js";
 
 function Directories({ url, setUrl, path, setPath }) {
-
-  const [product, setProduct] = useState([])
-  const [error, setError] = useState("")
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     try {
-      axios.get(url, { headers: authHeaders })
-        .then(response => {
-          setProduct(response.data)
+      axios
+        .get(url, { headers: authHeaders })
+        .then((response) => {
+          setProduct(response.data);
         })
-        .catch(err => {
-          setError(err.code)
-        })
+        .catch((err) => {
+          setError(err.code);
+        });
     } catch (e) {
-      setError(e.toString())
-      console.log("why")
+      setError(e.toString());
+      console.log("why");
     }
-  }, [url])
+  }, [url]);
 
   const handleSetPath = (newPath) => {
-    setPath(curr => [...curr, newPath])
-  }
+    setPath((curr) => [...curr, newPath]);
+  };
 
   function constructUrl(productItemName) {
-    const res = path.join('/')
-    return `${res}/${productItemName}`
+    const res = path.join("/");
+    return `${res}/${productItemName}`;
   }
 
   const handleCardClick = (productItemName) => {
-    handleSetPath(productItemName)
-    const newStr = constructUrl(productItemName)
-    const newName = `${newStr}/`
-    setUrl("https://api.github.com/repos/yellowHatpro/Codes-Practice/contents/" + newName + "?ref=master")
-  }
+    handleSetPath(productItemName);
+    const newStr = constructUrl(productItemName);
+    const newName = `${newStr}/`;
+    setUrl(
+      "https://api.github.com/repos/yellowHatpro/Codes-Practice/contents/" +
+      newName +
+      "?ref=master",
+    );
+  };
   if (error === NETWORK_ERROR_CODE) {
-    return <div className={"network-error"}>Please connect to internet</div>
+    return <div className={"network-error"}>Please connect to internet</div>;
   }
 
-  if (product.type === 'file') {
-    return <>
-      <File url={product?.download_url} />
-    </>
+  if (product.type === "file") {
+    return (
+      <>
+        <File url={product?.download_url} />
+      </>
+    );
   } else {
-    return (<>
-      <h2>{`${product?.length} Files and Folders`}</h2>
-      <div className='code-directories'>
-        {product.map((productItem, i) => (
-          <DisplayCard
-            key={i}
-            name={productItem.name}
-            type={productItem.type}
-            onClick={() => handleCardClick(productItem.name)} />
-        ))}
-      </div>
-    </>)
+    return (
+      <>
+        <h2>{`${product?.length} Files and Folders`}</h2>
+        <div className="code-directories">
+          {product.map((productItem, i) => (
+            <DisplayCard
+              key={i}
+              name={productItem.name}
+              type={productItem.type}
+              onClick={() => handleCardClick(productItem.name)}
+            />
+          ))}
+        </div>
+      </>
+    );
   }
 }
 export default Directories;
