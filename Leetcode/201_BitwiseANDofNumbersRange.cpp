4074165fa2ccc8f6125ce128c5aa67bc0f9701ec
yellowhatpro <yellowhatpro3119@gmail.com>
2024-02-21 23:49:25 +0530

    leetcode

---
+++ Leetcode/201_BitwiseANDofNumbersRange.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int rangeBitwiseAnd(int left, int right) {
+    int cnt = 0;
+    while (left != right) {
+      left >>= 1;
+      right >>= 1;
+      cnt++;
+    }
+    return (left << cnt);
+  }
+};
