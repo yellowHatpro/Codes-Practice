58132d1b4d019445d57d8c1705881cee82cf911d
yellowhatpro <yellowhatpro3119@gmail.com>
2024-03-18 00:03:13 +0530

    leetcode

---
+++ Leetcode/57_InsertInterval.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> insert(vector<vector<int>> &inv, vector<int> &ninv) {
+    vector<vector<int>> ans;
+    int start = ninv[0], end = ninv[1];
+    bool inserted = false;
+    for (int i = 0; i < size(inv); i++) {
+      int cstart = inv[i][0], cend = inv[i][1];
+
+      if (cend < start || inserted) {
+        ans.push_back({cstart, cend});
+        continue;
+      }
+      start = min(start, cstart);
+      if (end < cstart) {
+        ans.push_back({start, end});
+        ans.push_back({cstart, cend});
+        inserted = true;
+        continue;
+      }
+      if (end <= cend) {
+        ans.push_back({start, cend});
+        inserted = true;
+      }
+    }
+    if (!inserted) {
+      ans.push_back({start, end});
+    }
+    return ans;
+  }
+};
