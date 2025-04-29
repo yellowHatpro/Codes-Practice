a4b6d805f9e2eb6192b3bb40537342380fa7ea51
yellowhatpro <yellowhatpro3119@gmail.com>
2025-04-29 23:40:32 +0530

    lc 2962 sliding window brrr

--- Leetcode/2962_CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cpp
+++ Leetcode/2962_CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cpp
@@ -21,3 +21,21 @@ public:
     return ans;
   }
 };
+
+class SolutionAttempt2 {
+public:
+  long long countSubarrays(vector<int> &nums, int k) {
+    long long ans = 0;
+    int ele = *max_element(nums.begin(), nums.end());
+    int cnt = 0;
+    for (int r = 0, l = 0; r < nums.size(); r++) {
+      cnt += (nums[r] == ele);
+      while (cnt >= k) {
+        cnt -= (nums[l] == ele);
+        l++;
+      }
+      ans += l;
+    }
+    return ans;
+  }
+};
