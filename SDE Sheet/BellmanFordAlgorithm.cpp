af0d8666f3f53b3c69be2b4f19a8eda11db0cd44
yellowhatpro <yellowhatpro3119@gmail.com>
2025-12-15 00:39:36 +0530

    test commit on new arch desktop

--- SDE Sheet/BellmanFordAlgorithm.cpp
+++ SDE Sheet/BellmanFordAlgorithm.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
-#include <climits>
 using namespace std;
 
-//Single source shorted path
-// For directed graphs : but we can convert undirected graph to directed
-// Works for negetive weights
-// Can identify negetive cycles
+// Single source shorted path
+//  For directed graphs : but we can convert undirected graph to directed
+//  Works for negetive weights
+//  Can identify negetive cycles
 
 int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
-  vector<int> dist(n+1,1000000000);
+  vector<int> dist(n + 1, 1000000000);
   dist[src] = 0;
-  for(int i = 0; i <n-1; i++){
-    for(auto edge: edges){
+  for (int i = 0; i < n - 1; i++) {
+    for (auto edge : edges) {
       int u = edge[0];
       int v = edge[1];
       int wt = edge[2];
-      if (dist[u]!=1000000000 && dist[u]+wt<dist[v]) dist[v] = dist[u]+wt;
+      if (dist[u] != 1000000000 && dist[u] + wt < dist[v])
+        dist[v] = dist[u] + wt;
     }
   }
   return dist[dest];
